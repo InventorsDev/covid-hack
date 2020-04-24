421fbdaa69baf5fc21f4363970fe50a1fd7d9e39
Olawanle Joel <57611810+olawanlejoel@users.noreply.github.com>
2020-04-24 21:45:54 +0000

    Fixed in Jesus Name

--- js/app.js
+++ js/app.js
@@ -79,21 +79,6 @@ function getStateReport(){
 // Self test script
 var $$ = Dom7;
 
-var app = new Framework7({ 
-				// App root element
-				 root: '#app', // App Name 
-				 name: 'My App', // App id 
-				 id: 'com.myapp.test', // Enable swipe panel 
-				 panel: { swipe: 'left', }, // Add default routes 
-				 routes: [ { path: '/about/', url: 'about.html', }, ], // ... other parameters 
-				 });
-				 
-
-	
-	var $$ = Dom7;
-
-
-
 document.getElementById("show2").style.display = "none";
 document.getElementById("show3").style.display = "none";
 document.getElementById("finalButton").style.display = "none";
